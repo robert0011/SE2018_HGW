88c9fad117c11d68f9e017882fea44daa06b7e1c
robert0011 <39350155+robert0011@users.noreply.github.com>
2018-06-04 15:40:52 +0200

    .

--- DrawPanel.java
+++ DrawPanel.java
@@ -126,7 +126,14 @@ public class DrawPanel extends JPanel
 		{
 			throw new IllegalArgumentException("Ids must be valid");
 		}
-		lineindex = cid1 + cid2;
+		if(cid1 < cid2)
+		{
+			lineindex = ((cid1*50) + cid2);
+		}
+		else
+		{
+			lineindex = ((cid2*50) + cid1);
+		}
 		lines.set(lineindex,new Line(circles.get(cid1), circles.get(cid2)));
 
 		repaint();
@@ -138,7 +145,14 @@ public class DrawPanel extends JPanel
 		{
 			throw new IllegalArgumentException("Ids must be valid");
 		}
-		lineindex = cid1 + cid2;
+		if(cid1 < cid2)
+		{
+			lineindex = ((cid1*50) + cid2);
+		}
+		else
+		{
+			lineindex = ((cid2*50) + cid1);
+		}
 		lines.remove(lineindex);
 		lines.set(lineindex, new Line( new Circle(0,0,0), new Circle(0,0,0)));
 		repaint();
